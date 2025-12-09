1b9c5ce93f18a1399b51bf4a321affbae8a31d4a
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2025-12-09 20:47:06 +0900

    feat: 엔터티 설계, +BaseTimeEntity

---
+++ src/main/java/com/auction/auction/domain/auction/entity/Product.java
@@ -0,0 +1,42 @@
+package com.auction.auction.domain.auction.entity;
+
+import com.auction.auction.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "product")
+public class Product extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price; // 가격 or 경매 시작가
+
+    @Column(nullable = false)
+    private Integer stock; // 남은 수량 (한정수량, 경매=1)
+
+    @Builder
+    public Product(String name, Long price, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public void decreaseStock(int quantity) {
+        if (this.stock - quantity < 0) {
+            throw new IllegalArgumentException("재고 부족");
+        }
+        this.stock -= quantity;
+    }
+}
